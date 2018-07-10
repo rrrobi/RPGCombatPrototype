cacfd0a82f3b72a2f7aca20c508f7dbc4cb77b13
rrrobi <rrrrobi@msn.com>
2018-07-10 23:17:57 +0100

    Update to UI
    Panels set up, USer input class set up, button clicks work

---
+++ Assets/Scripts/Controllers/BattleUIController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BattleUIController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CharcterSelectbuttonPressed()
+    {
+        Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+    }
+
+    public void ActionSelectbuttonPressed()
+    {
+        Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+    }
+
+    public void EnemySelectbuttonPressed()
+    {
+        Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+    }
+}
