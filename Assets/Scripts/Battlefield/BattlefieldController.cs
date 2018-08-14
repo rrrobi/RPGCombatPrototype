1578531964d5eda175093593d7cc57c45a651b5b
rrrobi <rrrrobi@msn.com>
2018-08-14 21:27:05 +0100

    started refactoring battlefield and unit slots

---
+++ Assets/Scripts/Battlefield/BattlefieldController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct UnitSlot
+{
+    Vector3 Position;
+    bool IsOccupied;
+}
+
+public class BattlefieldController
+{
+    UnitSlot[,] FriendlySlots;
+    UnitSlot[,] EnemySlots;
+
+    public UnitSlot GetFriendlySlot(int column, int row)
+    {
+        return FriendlySlots[row, column];
+    }
+    public UnitSlot GetEnemySlot(int column, int row)
+    {
+        return EnemySlots[row, column];
+    }
+
+    int rows = 2;
+    int columns = 3;
+
+    public void Setup()
+    {
+        FriendlySlots = new UnitSlot[columns, rows];
+        EnemySlots = new UnitSlot[columns, rows];
+    }
+
+}
