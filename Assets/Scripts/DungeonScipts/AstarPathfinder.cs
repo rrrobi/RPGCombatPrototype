76502b44ed5f6de835462b03e3ddbfad07e9a5d2
rrrobi <rrrrobi@msn.com>
2019-01-29 13:19:41 +0000

    setting up for dungeon gen adding to main project

--- Assets/Scripts/DungeonScipts/AstarPathfinder.cs
+++ Assets/Scripts/DungeonScipts/AstarPathfinder.cs
@@ -2,15 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AstarPathfinder : MonoBehaviour {
+public class AstarPathfinder 
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
--- Assets/Scripts/DungeonScipts/BSP_MapGen.cs
+++ Assets/Scripts/DungeonScipts/BSP_MapGen.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BSP_MapGen : MonoBehaviour {
+public class BSP_MapGen
+{
+    // BSP - partition the space up into randomly sized areas
+    #region
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    #endregion
+
+    // Room placement - Asign a randomly sized room to each of the BSP segments
+    // The room can not be larger than the segemnt, and is positioned randomly within the segment, 
+    // it can not be placed so that it 'spills' out of the segment
+    #region
+
+    #endregion
+
+    // First pass of corridor connections - Pair up a number of the rooms previously created
+    // Use A* pathfinding to create a path between each pair.
+    #region
+
+    #endregion
+
+    // Second pass of corridor connections - Assign an entrance tile in one of the rooms,
+    // Ensure there is a path from this location to every other room
+    // Assign of of the distant rooms to contain the exit.
+    #region
+
+    #endregion
 }
