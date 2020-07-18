1ef7a30ead2e6acf3e79d6c0ca9cf01957ecb632
rrrobi <rrrrobi@msn.com>
2020-07-18 02:31:26 +0100

    work towards save file

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData 
+{
+    // Encrypted Binary save data
+    // Persists between game sessions
+
+
+    #region Hero Data
+    public string PlayerName = "";
+    public string CombatLevel = "";
+    public int MaxHP = 0;
+    public int CurrentHP = 0;
+        
+    public string StrengthModifier = "";
+    public string WillModifier = "";
+
+    public int GoldOwned = 0;
+    #endregion
+
+    #region Dungeon Data
+
+    #endregion
+}
---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    
+}
