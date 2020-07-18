8cabc64257ac2325418c52b3c19bc3485b4701ed
rrrobi <rrrrobi@msn.com>
2020-07-18 15:17:54 +0100

    work towards saving data

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Global;
 
 [System.Serializable]
 public class SaveData 
 {
     // Encrypted Binary save data
     // Persists between game sessions
-
+    string Date;
+    string Time;
 
     #region Hero Data
+        // TODO... implement a check to ensure Hero info is not different in layout to hero save info - 
+        // This could be a check, or it could simply use the same resource... look into it
     public string PlayerName = "";
     public string CombatLevel = "";
     public int MaxHP = 0;
@@ -24,4 +29,23 @@ public class SaveData
     #region Dungeon Data
 
     #endregion
+
+    #region World Data - outside world does not yet exist!
+    #endregion
+
+    public SaveData(HeroInfo hi, string dungeonData_placeholder)
+    {
+        Date = DateTime.Now.Date.ToString("DD-MM-YYYY");
+        Time = DateTime.Now.TimeOfDay.ToString("HH:mm:ss");
+
+        PlayerName = hi.PlayerName;
+        CombatLevel = hi.CombatLevel;
+        MaxHP = hi.MaxHP;
+        CurrentHP = hi.CurrentHP;
+        StrengthModifier = hi.StrengthModifier;
+        WillModifier = hi.WillModifier;
+        GoldOwned = hi.GoldOwned;
+
+        // Dungeon Data
+    }
 }
--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -6,5 +6,30 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public static class SaveSystem
 {
-    
+    public static void FullSave()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + $"/{GameManager.Instance.GetActiveSaveSlot().ToString()}Save.slot";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        SaveData data = new SaveData(GameManager.Instance.GetHeroData.heroWrapper.HeroData.HeroInfo, "Dungeon placeholder");
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static SaveData LoadFull()
+    {
+        string path = Application.persistentDataPath + $"/{GameManager.Instance.GetActiveSaveSlot().ToString()}Save.slot";
+        if (File.Exists(path))
+        {
+
+        }
+        else
+        {
+            Debug.LogError($"Save file not found: {path}");
+            return null;
+        }
+
+    }
 }
